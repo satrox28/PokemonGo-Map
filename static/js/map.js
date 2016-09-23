b7aa254920cf904a0e3b9c3b24dc9a9a5c450be0
Allen Piscitello <allen.piscitello@gmail.com>
2016-09-22 20:25:25 -0500

    Fixed rebase merge errors

--- static/js/map.js
+++ static/js/map.js
@@ -37,7 +37,6 @@ var searchMarker
 var storeZoom = true
 var scanPath
 
-
 var selectedStyle = 'light'
 
 var updateWorker
@@ -499,26 +498,6 @@ function spawnpointLabel (item) {
       </div>`
   }
   return str
-}
-
-function getGoogleSprite (index, sprite, displayHeight) {
-  displayHeight = Math.max(displayHeight, 3)
-  var scale = displayHeight / sprite.iconHeight
-  // Crop icon just a tiny bit to avoid bleedover from neighbor
-  var scaledIconSize = new google.maps.Size(scale * sprite.iconWidth - 1, scale * sprite.iconHeight - 1)
-  var scaledIconOffset = new google.maps.Point(
-    (index % sprite.columns) * sprite.iconWidth * scale + 0.5,
-    Math.floor(index / sprite.columns) * sprite.iconHeight * scale + 0.5)
-  var scaledSpriteSize = new google.maps.Size(scale * sprite.spriteWidth, scale * sprite.spriteHeight)
-  var scaledIconCenterOffset = new google.maps.Point(scale * sprite.iconWidth / 2, scale * sprite.iconHeight / 2)
-
-  return {
-    url: sprite.filename,
-    size: scaledIconSize,
-    scaledSize: scaledSpriteSize,
-    origin: scaledIconOffset,
-    anchor: scaledIconCenterOffset
-  }
 }
 
 function addRangeCircle (marker, map, type, teamId) {
@@ -1709,19 +1688,6 @@ $(function () {
   }
 
   // Setup UI element interactions
-  $('#gyms-switch').change(function () {
-    var options = {
-      'duration': 500
-    }
-    var wrapper = $('#gyms-filter-wrapper')
-    if (this.checked) {
-      wrapper.show(options)
-    } else {
-      wrapper.hide(options)
-    }
-    return buildSwitchChangeListener(mapData, ['gyms'], 'showGyms').bind(this)()
-  })
-
   $('#pokemon-switch').change(buildSwitchChangeListener(mapData, ['pokemons'], 'showPokemon'))
   $('#scanned-switch').change(buildSwitchChangeListener(mapData, ['scanned'], 'showScanned'))
   $('#spawnpoints-switch').change(buildSwitchChangeListener(mapData, ['spawnpoints'], 'showSpawnpoints'))
